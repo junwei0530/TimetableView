3ea30e450d2354d9b0f8043abbaa1105ec576ab1
junwei0530 <junwei9810@1utar.my>
2019-12-19 12:48:17 +0800

    Update Schedule.java

--- timetableview/src/main/java/com/github/tlaabs/timetableview/Schedule.java
+++ timetableview/src/main/java/com/github/tlaabs/timetableview/Schedule.java
@@ -3,17 +3,20 @@ package com.github.tlaabs.timetableview;
 import java.io.Serializable;
 
 public class Schedule implements Serializable {
-    static final int MON = 0;
-    static final int TUE = 1;
-    static final int WED = 2;
-    static final int THU = 3;
-    static final int FRI = 4;
-    static final int SAT = 5;
-    static final int SUN = 6;
+    static final int MON = 1;
+    static final int TUE = 2;
+    static final int WED = 3;
+    static final int THU = 4;
+    static final int FRI = 5;
+    static final int SAT = 6;
+    static final int SUN = 7;
 
     String classTitle="";
     String classPlace="";
     String professorName="";
+    String HourPay="";
+    
+    private int available = 1;
     private int day = 0;
     private Time startTime;
     private Time endTime;
@@ -21,6 +24,22 @@ public class Schedule implements Serializable {
     public Schedule() {
         this.startTime = new Time();
         this.endTime = new Time();
+    }
+    
+    public int getAvailable(){
+        return available;
+    }
+    
+    public void setAvailable(int available){
+        this.available = available;
+    }
+    
+    public String getHourPay(){
+        return HourPay;
+    }
+    
+    public void setHourPay(String HourPay){
+        this.HourPay = HourPay;
     }
 
     public String getProfessorName() {
@@ -34,7 +53,7 @@ public class Schedule implements Serializable {
     public String getClassTitle() {
         return classTitle;
     }
-
+    
     public void setClassTitle(String classTitle) {
         this.classTitle = classTitle;
     }
